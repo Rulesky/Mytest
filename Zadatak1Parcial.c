4a0c6742023ea6303bb55fefed3de9c315e75596
Rulesky <mroadin@gmail.com>
2018-12-20 06:42:48 -0800

    adding files for exercise

---
+++ Zadatak1Parcial.c
@@ -0,0 +1,10 @@
+double pov;
+#include "zadatak1.h"
+#include <stdio.h>
+
+void main (void)
+{
+	int radijus = 10;
+	
+	povrsina (radijus);
+	printf (txt1, pov);
---
+++ Zdatak1Parcial.h
@@ -0,0 +1,7 @@
+#define txt1 "\n Površina kruga radijusa 10 je %f"
+#define PI 3.14
+#define KVADRAT(x) (x) * (x)
+void povrsina (int r)
+{
+	pov = KVADRAT(r) * PI;
+}
